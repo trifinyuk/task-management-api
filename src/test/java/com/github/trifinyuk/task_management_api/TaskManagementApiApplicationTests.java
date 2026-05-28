0be4d58b1d73747c524f7d3681c94972be8f94af
trifinyuk <trifinyuk@gmail.com>
2026-05-28 18:32:17 +0300

    chore: initialize Spring Boot project with Java 21

---
+++ src/test/java/com/github/trifinyuk/task_management_api/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.trifinyuk.task_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
