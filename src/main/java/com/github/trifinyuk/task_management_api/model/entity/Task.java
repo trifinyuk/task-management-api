ce7ec6fe45948edb5e189bea7e149b5c020cbe43
trifinyuk <trifinyuk@gmail.com>
2026-05-29 11:29:44 +0300

    feat: create Task entity with JPA annotations

---
+++ src/main/java/com/github/trifinyuk/task_management_api/model/entity/Task.java
@@ -0,0 +1,39 @@
+package com.github.trifinyuk.task_management_api.model.entity;
+
+import com.github.trifinyuk.task_management_api.model.enums.TaskStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.OffsetDateTime;
+
+@Entity
+@Table(name = "tasks")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(length = 4000)
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TaskStatus status;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private OffsetDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = OffsetDateTime.now();
+    }
+}
